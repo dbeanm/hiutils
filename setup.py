6c6efde2b11230699ddd36b0924d2cab2c465466
Daniel Bean <daniel.bean@kcl.ac.uk>
2022-02-16 13:40:16 +0000

    bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="hiutils",
-    version="0.2.0",
+    version="0.2.1",
     author="Dan Bean",
     author_email="daniel.bean@kcl.ac.uk",
     description="Utilities for health informatics",
